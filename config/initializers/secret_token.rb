6040d1d8ca966bd3a5165811550185ea057a44aa
jcap49 <jcapecelatro@gmail.com>
2013-09-06 20:24:05 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IssRace::Application.config.secret_key_base = 'cf5ad3286afe58b8269b116991e30939226187b3e3a39e5bd1e2f261f195af5f0729209a800ef6cf8239a0bc15399135add560dd245cd1c7fb0d2029de2857d5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IssRace::Application.config.session_store :cookie_store, key: '_iss_race_session'
